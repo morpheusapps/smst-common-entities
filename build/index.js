783dedf2b0cdb45af17ed538f77cc09ffc2b34c4
maorRoz <andory@post.bgu.ac.il>
2019-07-26 16:57:47 +0300

    init2

--- build/index.js
+++ build/index.js
@@ -1,5 +1,5 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+exports.__esModule = true;
 var meow_1 = require("./meow");
 var greeter = function (person) { return "Hello " + meow_1.meow() + " " + person + "!"; };
 var name = 'Node Hero';
