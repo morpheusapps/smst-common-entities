bb9acf2cd578b3e64b135aedb33ab9e60dd3d726
maorRoz <andory@post.bgu.ac.il>
2019-07-26 19:09:04 +0300

    fixed eslint for typescript

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { meow } from './meow';
 
 const greeter = (person: string) => `Hello ${meow()} ${person}!`;
 
-const name = 'Node Hero';
+const name = `Node Hero`;
 
+// eslint-disable-next-line no-console
 console.log(greeter(name));
