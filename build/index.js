1b15865d9bd600b09795901ea38d260ed35e9f8f
maorRoz <andory@post.bgu.ac.il>
2019-07-27 20:27:55 +0300

    removed build/index.js

--- build/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var meow_1 = require("./meow");
-var greeter = function (person) { return "Hello " + meow_1.meow() + " " + person + "!"; };
-var name = 'Node Hero';
-console.log(greeter(name));
