3b2e3cd38fc4d7002d404c5ada59e8d7a7d69f56
maorRoz <andory@post.bgu.ac.il>
2019-07-27 14:03:30 +0300

    added logger

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -1,4 +1,29 @@
-/* eslint-disable no-console */
-export default {
-  log: (logMessage: string): void => console.log(logMessage)
-};
+import winston from 'winston';
+
+const myFormat = winston.format.printf(
+  ({ timestamp, level, message }): string => `${level}: ${timestamp} ${message}`
+);
+
+const logger = winston.createLogger({
+  level: 'info',
+  transports: [
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' })
+  ]
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.timestamp({
+          format: 'DD-MM-YYYY HH:mm:ss'
+        }),
+        winston.format.colorize(),
+        myFormat
+      )
+    })
+  );
+}
+
+export default logger;
