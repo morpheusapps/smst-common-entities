323914817705db3299cb1992e3cb842868f2e387
maorRoz <andory@post.bgu.ac.il>
2019-10-13 16:44:23 +0300

    improved Fakes

--- tests-utils/Fakes.ts
+++ tests-utils/Fakes.ts
@@ -1,16 +1,32 @@
 import faker from 'faker';
 import { Sample } from './Sample';
 
+interface OptionalArgs {
+  withNull?: boolean;
+}
+
 export const Fakes = {
   number: (): number => faker.random.number(),
   boolean: (): boolean => faker.random.boolean(),
   string: (): string => faker.lorem.words(),
-  numberOptional: (): number | undefined =>
-    Sample([faker.random.number(), undefined]),
-  booleanOptional: (): boolean | undefined =>
-    Sample([faker.random.boolean(), undefined]),
-  stringOptional: (): string | undefined =>
-    Sample([faker.lorem.words(), undefined]),
+  numberOptional: (args?: OptionalArgs): number | undefined | null =>
+    Sample(
+      args && args.withNull
+        ? [faker.random.number(), undefined, null]
+        : [faker.random.number(), undefined]
+    ),
+  booleanOptional: (args?: OptionalArgs): boolean | undefined | null =>
+    Sample(
+      args && args.withNull
+        ? [faker.random.boolean(), undefined, null]
+        : [faker.random.boolean(), undefined]
+    ),
+  stringOptional: (args?: OptionalArgs): string | undefined | null =>
+    Sample(
+      args && args.withNull
+        ? [faker.lorem.words(), undefined, null]
+        : [faker.lorem.words(), undefined]
+    ),
   uuid: (): string => faker.random.uuid(),
   route: (): string => `/${faker.internet.domainWord()}`
 };
